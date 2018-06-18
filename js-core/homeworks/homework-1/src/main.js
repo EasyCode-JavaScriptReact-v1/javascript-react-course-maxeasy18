5086731742e18bab07fdf3175d88a4c1ebab1f59
Maaxym <maxym.marchenko@gmail.com>
2018-06-18 22:42:43 +0300

    Домашнее задание с первого урока как я его понял.

---
+++ js-core/homeworks/homework-1/src/main.js
@@ -0,0 +1,24 @@
+var userName = "Albert Einstain"; // string
+var isReady = false; //boolean
+var phoneNumber = 1111111; //number 
+var nullField = null; //null
+var emptyField = undefined; //undefined
+var parents = {
+		"father" : "unknown",
+		"mother" : " Elizabet Taylor"
+	}; // object 
+var friends = ['Boris', 'Stepan', 'Taras']; //array (object)
+
+var user = {
+	"isReady" : isReady,
+	"phoneNumber" : phoneNumber,
+	"userName" : userName,
+	"nullField" : nullField,
+	"emptyField" : emptyField,
+	"parents" : parents,
+	"friends" : friends,
+	"symbol" : Symbol('a')
+
+
+}
+console.log(user);
